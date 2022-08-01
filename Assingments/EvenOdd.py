779aed5db4e3d88d3fee014e509f946dd531cf71
arfan2593 <irfanntdc4@gmail.com>
2022-08-01 14:38:12 +0500

    Create EvenOdd.py
    Program to check the given number is even or odd

---
+++ Assingments/EvenOdd.py
@@ -0,0 +1,8 @@
+print("**********Even / Odd Numbers**********")
+
+number= int(input("Please enter the Number: "))
+
+if (number%2==0):
+    print("Your number is an Even Number")
+else:
+    print("Your number ia an Odd Number")    
